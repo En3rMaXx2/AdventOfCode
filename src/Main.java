c8fdc28d35d5a14c7a45762d91218981073012c0
Fabio <fabioleidheuser@gmail.com>
2022-12-07 15:45:58 +0100

    day 7 WIP

--- src/Main.java
+++ src/Main.java
@@ -20,15 +20,20 @@ public class Main {
         System.out.println("\nDay 4");
         System.out.println(Day4.day4No1());
         System.out.println(Day4.day4No2());
-        */
+
 
         System.out.println("\nDay 5");
         System.out.println(Day5.day5No1());
         System.out.println(Day5.day5No2());
 
-        //System.out.println("\nDay 6");
-        //System.out.println(Day6.day6No1());
-        //System.out.println(Day6.day6No2());
+        System.out.println("\nDay 6");
+        System.out.println(Day6.day6No1());
+        System.out.println(Day6.day6No2());
+        */
+
+        System.out.println("\nDay 7");
+        System.out.println(Day7.day7No1());
     }
+
 
 }
---
+++ src/days/Day7.java
@@ -0,0 +1,50 @@
+package days;
+
+import java.io.IOException;
+
+import service.TextReader;
+
+public class Day7 {
+
+    public static int day7No1() throws IOException {
+        String[] array = TextReader.textFileToArray("C:/Projekte/AdventOfCode/src/resources/Day7Example.txt");
+        int dirSum =0;
+        int endSum=0;
+
+
+        for (int i=1; i<array.length-1; i++) {
+            String[] split = array[i].split(" ");
+
+
+            if(split[0].equals("$")){
+                if(dirSum < 100000) {
+                    endSum+= dirSum;
+                    System.out.println(dirSum);
+                }
+                dirSum =0;
+            }
+            else if(split[0].equals("dir")) {
+
+            }
+            else {
+
+
+
+                try {
+                    dirSum += Integer.parseInt(split[0]);
+                }
+                catch (NumberFormatException e) {
+                    //nothing
+                }
+
+
+
+
+
+            }
+
+        }
+
+        return endSum;
+    }
+}
